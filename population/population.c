d0ede5c3f0238c6240aba9cc55390f1797556a3e
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-08-31 11:00:45 +0000

    Thu, Aug 31, 2023, 11:00 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,22 +4,22 @@
 
 int main(void)
 {
-    int S;
-    int E;
+    int n;
+    int m;
     // TODO: Prompt for start size
 do
 {
-    S = get_int("Start size: ");
+    n = get_int("Start size: ");
 }
-while (n < 9);
+while (S < 9);
     // TODO: Prompt for end size
 do
 {
-    E = get_int("End size: ");
+    m = get_int("End size: ");
 }
 while (m < n);
     // TODO: Calculate number of years until we reach threshold
-int n = S;
+
 float end year size;
 end year size = n + (n/3) - (n/4);
     // TODO: Print number of years
