9132fe224ab21b1571c3b490b037dfee4725464d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-01 13:17:53 +0000

    Fri, Sep 1, 2023, 3:17 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,9 +14,9 @@ int main(void)
     printf("Stored: %i\n", n);
 
     // Printing the pyramid blocks(#)
-     for (int i = 0; i < n; i++)
+     for (int i = 1; i <= n; i++)
          {
-            for (int j = 0; j < n; j++)
+            for (int j = 1; j <= i; j++)
             {
                 printf ("#");
             }
