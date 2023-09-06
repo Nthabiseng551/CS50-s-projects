1ef354429e2f571b7f6399a97a9d6abbb811aac5
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-06 09:03:39 +0000

    Wed, Sep 6, 2023, 11:03 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,14 +8,17 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
     int letters = count_letters(text);
     int words = count_words(text);
+    int sentences = count_sentences(text);
     printf("%i letters\n", letters);
-     printf("%i words\n", words);
+    printf("%i words\n", words);
+    printf("%i sentences\n", sentences);
 
 
 }
