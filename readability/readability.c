a105d8bae77626375e3aa95b7eb9127dc803d7c1
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-06 09:25:40 +0000

    Wed, Sep 6, 2023, 11:25 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,7 +16,8 @@ int main(void)
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
-    
+
+    L = (letters/words) * 100
 
     index = round(0.0588 * L - 0.296 * S - 15.8);
 
