b1f1482ad5438361c3280a7f2df9301ef73ee5b6
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-06 08:06:58 +0000

    Wed, Sep 6, 2023, 10:06 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,8 +3,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text)
+
 int main(void)
 {
     string text = get_string("Text: ");
     printf("Text: %s\n",text);
+}
+
+// Function to count letters
+int count_letters(string text)
+{
+
 }
