e639855dc3fe5b49707904fc3247ca715b1a76d6
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-14 13:06:45 +0000

    Thu, Sep 14, 2023, 3:06 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -126,10 +126,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
-            }
             image [i][j].rgbtRed = round(totalRed / counter);
             image [i][j].rgbtGreen = round(totalGreen / counter);
             image [i][j].rgbtBlue = round(totalBlue / counter);
+            }
+        }
 
     return;
 }
