786c693eb4db3a21280d0a3c1ab8bfa2f6c8b27e
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-14 13:23:44 +0000

    Thu, Sep 14, 2023, 3:23 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -94,6 +94,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    //TODO
+    // TODO
     return;
 }
