58ee4d8f1c6e948f58db283ef90dc4a81f0e229d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-14 11:25:35 +0000

    Thu, Sep 14, 2023, 1:25 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -98,8 +98,30 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
+RGBTRIPLE temp[height][width];
 {
-    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed, totalGreen, totalBlue;
+            totalRed = totalGreen = totalBlue = 0;
+            float counter = 0.00;
 
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1))
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
     return;
 }
