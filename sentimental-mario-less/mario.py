fc01143d4ccd1d3f6431fe4fc98543bf5d6e87bf
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-23 01:27:31 +0000

    Sat, Sep 23, 2023, 3:27 AM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,8 +1,13 @@
 from cs50 import get_int
 
-while True:
-    n = get_int("Height: ")
-    if n >= 1 and n <= 8:
-        break
-return n
+def main():
+    height = get_height()
+    for i in range(height):
+
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        if n >= 1 and n <= 8:
+            break
+    return n
 
