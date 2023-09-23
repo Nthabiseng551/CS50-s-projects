a4c6b84512691186c922884b4b8f66f443306ef1
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-23 01:48:11 +0000

    Sat, Sep 23, 2023, 3:48 AM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,8 +2,8 @@ from cs50 import get_int
 
 def main():
     height = get_height()
-    for i in range(height):
-        for j in range(i, height + 1):
+    for i in range(1, height + 1):
+        for j in range(i, height):
             print(" ", end="")
         for k in range(1, i + 1):
             print("#", end="")
