ed4a1e107af2cd4b18691f2667abe80b2d85b593
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-23 02:09:17 +0000

    Sat, Sep 23, 2023, 4:09 AM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,6 +1,32 @@
 from cs50 import get_float
 
 def main():
+    # Ask how many cents the customer is owed
+    cents = get_cents()
+
+    # Calculate the number of quarters to give the customer
+    quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
+
+    # Calculate the number of dimes to give the customer
+    dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
+    # Calculate the number of nickels to give the customer
+    nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
+
+    # Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    # Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    # Print total number of coins to give the customer
+    printf("%i\n", coins);
+
+
 def get_cents():
 def calculate_quarters():
 def calculate_dimes():
