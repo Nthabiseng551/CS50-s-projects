5d6485d0f512b07804eb8422c2d7e3e153a5b6eb
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-23 03:00:39 +0000

    Sat, Sep 23, 2023, 5:00 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,25 @@
 from cs50 import get_string
 
 def main():
+    text = get_string("Text: ")
+    letters = count_letters(text)
+    words = count_words(text)
+    sentences = count_sentences(text)
+
+    L = (letters / words) * 100
+    S = (sentences / words) * 100
+
+    index = int(round((0.0588 * L) - (0.296 * S) - 15.8))
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
+
+# Function to count letters in a text
 def count_letters(text):
+
+
 def count_words(text):
 def count_sentences(text):
