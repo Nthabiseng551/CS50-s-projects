373be7ebade905e3f5936fa894798972a823b8ae
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 14:03:15 +0000

    Sat, Sep 30, 2023, 4:03 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -16,7 +16,7 @@ SELECT id, destination_airport_id, hour, minute FROM flights WHERE origin_airpor
 -- bank accounts to get suspects' person ids from the account numbers obtained;
 -- phone calls to check callers at the day and time of crime with call duration of less than a minute;
 --then identify suspects that matched the data
-SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=36);
+SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 25 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=36);
 
 
 -- The city the thief escaped to found from the destination airport id and airports table
