1ddecfb6131324ef81837f43fb99a10abbaccfdd
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 14:13:34 +0000

    Sat, Sep 30, 2023, 4:13 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,14 +3,23 @@
 -- Getting the description of the crime from crime scene reports data
 SELECT description
 FROM crime_scene_reports
-WHERE year=2021 AND month=7 AND day=28 AND street = "Humphrey Street";
+WHERE year=2021 AND month=7
+AND day=28 AND street = "Humphrey Street";
 
 -- Since description mentions that interviews where conducted on three witnesses, we are going to check the interview data for more information
 -- Get names and transcripts of the witnesses
-SELECT name, transcript FROM interviews WHERE month=7 AND day=28 AND year=2021 AND transcript LIKE '%bakery%'; -- all witnesses mentioned 'bakery'
+SELECT name, transcript
+FROM interviews WHERE month=7 AND day=28
+AND year=2021 AND transcript LIKE '%bakery%'; -- all witnesses mentioned 'bakery'
 
 -- check passengers of earliest flight out of fiftyville on July 29;
-SELECT id, destination_airport_id, hour, minute FROM flights WHERE origin_airport_id = (SELECT id FROM airports WHERE city= 'Fiftyville') AND year=2021 AND month=7 AND day=29;
+SELECT id, destination_airport_id, hour, minute
+FROM flights
+WHERE origin_airport_id =
+        (SELECT id
+        FROM airports
+        WHERE city= 'Fiftyville')
+AND year=2021 AND month=7 AND day=29;
 -- From the data above I selected the earliest flight (8:20), with flight id of 36 and destination airport id of 4
 
 -- Check the bakery security logs to get the license plate of suspects that left the bakery around a time frame 10 mins after theft;
@@ -18,7 +27,16 @@ SELECT id, destination_airport_id, hour, minute FROM flights WHERE origin_airpor
 -- bank accounts to get suspects' person ids from the account numbers obtained;
 -- phone calls to check callers at the day and time of crime with call duration of less than a minute;
 --then identify suspects that matched the data
-SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 25 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=36);
+SELECT name
+FROM people
+WHERE id IN
+    (SELECT person_id
+    FROM bank_accounts
+    WHERE account_number IN
+        (SELECT account_number
+        FROM atm_transactions
+        WHERE year=2021 AND month=7 AND day=28
+        AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 25 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=36);
 
 
 -- The city the thief escaped to found from the destination airport id and airports table
