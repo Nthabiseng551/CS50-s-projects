d0ef3d5c628ce173e395a6e6c2c90eb7c350cec9
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 13:36:01 +0000

    Sat, Sep 30, 2023, 3:36 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,5 +10,7 @@ SELECT name, transcript FROM interviews WHERE month=7 AND day=28 AND year=2021 A
 -- Check the bakery security logs to get the license plate of suspects that left the bakery around a time frame 10 mins after theft;
 -- atm transactions to get account numbers from which money was withdrawn at an atm on Leggett Street in the morning of the same day;
 -- bank accounts to get suspects' person ids from the account numbers obtained;
--- phone calls to check callers at the day and time of crime; then identify suspects that matched the data
-SELECT name, phone_number, passport_number FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit');
+-- phone calls to check callers at the day and time of crime with call duration of less than a minute; then identify suspects that matched the data
+SELECT name, phone_number, passport_number FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60);
+
+-- From Raymond's transcript, we are going to for ealiest flight out of fiftyville on july,29 2021
