3ecc7d79c2c0a00d4bbd4bae0c69bf5df9cd23d5
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 10:37:50 +0000

    Sat, Sep 30, 2023, 12:37 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id IN (SELECT personid FROM stars WHERE movieid IN (SELECT id FROM movies WHERE));
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE (people.name = "Kevin Bacon" AND people.birth = 1958) EXCEPT SELECT name FROM people WHERE name = "Kevin Bacon";
--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE year = 2010 ORDER BY rating DESC;
+SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year = 2010 ORDER BY ratings.rating DESC, movies.title;
