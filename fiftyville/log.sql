67d2bed0930d6a45a047a33f925020ed02c94f3d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 13:23:14 +0000

    Sat, Sep 30, 2023, 3:23 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,6 +7,7 @@ SELECT description FROM crime_scene_reports WHERE year=2021 AND month=7 AND day=
 -- Get names and transcripts of the witnesses
 SELECT name, transcript FROM interviews WHERE month=7 AND day=28 AND year=2021 AND transcript LIKE '%bakery%'; -- all witnesses mentioned 'bakery'
 
--- One witness (Ruth) saw the thief drive away from the bakery parking lot 10 minutes after theft so the bakery security logs are checked
--- to get the license plate of suspects that left the bakery around a time frame 10 mins after theft; 
+-- Check the bakery security logs to get the license plate of suspects that left the bakery around a time frame 10 mins after theft;
+-- atm transactions to get account numbers from which money was withdrawn at an atm on Leggett Street in the morning of the same day;
+-- bank accounts to get suspects' person ids from the account numbers obtained; then identify suspects 
 SELECT name, phonenumber, passportnumber FROM people WHERE id IN (SELECT personid FROM bankaccounts WHERE accountnumber IN (SELECT accountnumber FROM atmtransactions WHERE year2021 AND month7 AND day28 AND atmlocation "Leggett Street" AND transactiontype 'withdraw')) AND licenseplate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit');
