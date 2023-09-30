2c400d17f5492129ea531f850649166174cf13f2
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 13:50:50 +0000

    Sat, Sep 30, 2023, 3:50 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,8 +10,9 @@ SELECT name, transcript FROM interviews WHERE month=7 AND day=28 AND year=2021 A
 -- Check the bakery security logs to get the license plate of suspects that left the bakery around a time frame 10 mins after theft;
 -- atm transactions to get account numbers from which money was withdrawn at an atm on Leggett Street in the morning of the same day;
 -- bank accounts to get suspects' person ids from the account numbers obtained;
--- phone calls to check callers at the day and time of crime with call duration of less than a minute; then identify suspects that matched the data
-SELECT name, phone_number, passport_number FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60);
+-- phone calls to check callers at the day and time of crime with call duration of less than a minute;
+-- check passengers of earliest flight out of fiftyville on July 29; then identify suspects that matched the data
+SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year=2021 AND month=7 AND day=28 AND atm_location= "Leggett Street" AND transaction_type= 'withdraw')) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit') AND phone_number IN (SELECT caller FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=36);
 
 -- From Raymond's transcript, we are going to for ealiest flight out of fiftyville on july,29 2021
 SELECT id, destination_airport_id, hour, minute FROM flights WHERE origin_airport_id = (SELECT id FROM airports WHERE city= 'Fiftyville') AND year=2021 AND month=7 AND day=29;
