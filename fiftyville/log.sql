c39e23e98d3787f6b5c42ee036ad3694a4ce8c0c
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 13:15:17 +0000

    Sat, Sep 30, 2023, 3:15 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,4 +9,4 @@ SELECT name, transcript FROM interviews WHERE month=7 AND day=28 AND year=2021 A
 
 -- One witness (Ruth) saw the thief drive away from the bakery parking lot 10 minutes after theft so the bakery security logs are checked
 -- Getting the license plate of suspects that left the bakery around a time frame 10 mins after theft
-SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit';
+SELECT name, phonenumber, passportnumber FROM people WHERE id IN (SELECT personid FROM bankaccounts WHERE accountnumber IN (SELECT accountnumber FROM atmtransactions WHERE year2021 AND month7 AND day28 AND atmlocation "Leggett Street" AND transactiontype 'withdraw'))(SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit');
