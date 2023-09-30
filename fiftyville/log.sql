b0928ea893cb67f5b1bf55fccb37681af0c9805d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 14:08:41 +0000

    Sat, Sep 30, 2023, 4:08 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Getting the description of the crime from crime scene reports data
-SELECT description FROM crime_scene_reports WHERE year=2021 AND month=7 AND day=28 AND street = "Humphrey Street";
+SELECT description
+FROM crime_scene_reports
+WHERE year=2021 AND month=7 AND day=28 AND street = "Humphrey Street";
 
 -- Since description mentions that interviews where conducted on three witnesses, we are going to check the interview data for more information
 -- Get names and transcripts of the witnesses
