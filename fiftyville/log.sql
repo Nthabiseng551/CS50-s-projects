0e8982ce053225a82554b79700cec551b8bf3294
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-09-30 13:09:14 +0000

    Sat, Sep 30, 2023, 3:09 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- Getting the description of the crime from crime scene reports table
+-- Getting the description of the crime from crime scene reports data
 SELECT description FROM crime_scene_reports WHERE year=2021 AND month=7 AND day=28 AND street = "Humphrey Street";
 
--- since description mentions that interviews where conducted on three witnesses, we are going to check the inter
+-- Since description mentions that interviews where conducted on three witnesses, we are going to check the interview data for more information
+-- Get names and transcripts of the witnesses
+SELECT name, transcript FROM interviews WHERE month=7 AND day=28 AND year=2021 AND transcript LIKE '%bakery%'; -- all witnesses mentioned 'bakery'
+
+-- One witness (Ruth) saw the thief drive away from the bakery parking lot 10 minutes after theft so the bakery security logs are checked
+-- Getting the license plate of suspects that left the bakery around a time frame 10 mins after theft
+SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 35 AND activity = 'exit';
