9ed1998086754410f91f427aa15dc1c524f6c768
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-10-12 10:10:04 +0000

    Thu, Oct 12, 2023, 12:10 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -78,7 +78,7 @@ def buy():
             return apology("Insufficient cash")
 
         # Update user's table
-        db.execute("UPDATE users SET cash =")
+        db.execute("UPDATE users SET cash = ? WHERE id = ?", cash - total_cost, user_id)
 
         # Add purchase to the history table #come back
         db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (?, ?, ?, ?))", user_id, symbol=symbol, shares=shares, price=price)
