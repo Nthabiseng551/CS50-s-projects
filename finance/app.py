1563ead9c9cdb55f9e8ca91f6bb6e00df560b5a0
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-10-12 13:46:18 +0000

    Thu, Oct 12, 2023, 3:46 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -237,34 +237,20 @@ def sell():
     else:
         return render_template("sell.html", stocks=stocks)
 
-def password_check(passwd):
 
-    SpecialSym =['$', '@', '#', '%']
-    val = True
+    valid = True
 
-    if len(passwd) < 6:
-        print('length should be at least 6')
-        val = False
+    if len(password) < 8 or len(password) > 20:
+        print("The length of the password must be atleast 8 but not exceeding 20")
+        valid = False
 
-    if len(passwd) > 20:
-        print('length should be not be greater than 8')
-        val = False
+    if not any(char.isdigit() for char in password) :
+        print("Password must contain atleast one number")
+        valid = False
 
-    if not any(char.isdigit() for char in passwd):
-        print('Password should have at least one numeral')
-        val = False
+    if not any(char.isupper() for char in passwd) or not any(char.islower() for char in passwd):
+        print("Password must contain atleast one uppercase letter and atleast one lowercase letter")
+        valid = False
 
-    if not any(char.isupper() for char in passwd):
-        print('Password should have at least one uppercase letter')
-        val = False
-
-    if not any(char.islower() for char in passwd):
-        print('Password should have at least one lowercase letter')
-        val = False
-
-    if not any(char in SpecialSym for char in passwd):
-        print('Password should have at least one of the symbols $@#')
-        val = False
-    if val:
-        return val
- 
+    if valid:
+        return valid
