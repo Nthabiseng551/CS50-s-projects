b0cccb91cd8637ca8fc0614a59f8cb75b1858dc8
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-10-23 00:14:33 +0000

    Mon, Oct 23, 2023, 2:14 AM +02:00

--- project/app.py
+++ project/app.py
@@ -161,12 +161,14 @@ def form():
     users=db.execute("SELECT * FROM users WHERE id=?", user_id)
 
     if request.method == "POST":
-        # query database for request status of user and usernames
-        requests = db.execute("SELECT requests FROM users WHERE username=?", request.form.get("username"))[0]["requests"]
+
         usernames = db.execute("SELECT username FROM users")
         #check if username valid and if user already has request
         if request.form.get("username") not in usernames:
             return apology("please provide valid username")
+
+        # query database for request status of user and usernames
+        requests = db.execute("SELECT requests FROM users WHERE username=?", request.form.get("username"))[0]["requests"]
 
         if requests != 0:
             return apology("Each user is allowed one request at a time")
