7e8596dea2a6459232d2a61f637281c41d5b7566
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-10-29 02:18:23 +0000

    Sun, Oct 29, 2023, 4:18 AM +02:00

--- project/app.py
+++ project/app.py
@@ -32,12 +32,12 @@ CONCERNS = ["Grief and loss", "Self-Esteem/ Body image/ Self-confidance issues",
 @app.route("/")
 def index():
 
-    return render_template("index.html")
+    return render_template("index.html", )
 
 # Define index page for counsellors
 @app.route("/counsellor")
 def counsellor():
-
+    username=db.execute("SELECT username FROM users WHERE id=?", session["user_id"])[0]["username"]
     return render_template("counsellor.html", username=username)
 
 @app.route("/login", methods=["GET", "POST"])
