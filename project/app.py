9cd199d9eb84cd0374e7557d5d34f6a56b1b942b
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-12 01:03:53 +0000

    Sun, Nov 12, 2023, 3:03 AM +02:00

--- project/app.py
+++ project/app.py
@@ -324,7 +324,7 @@ def stories():
         concerns_csv = ','.join(checked_concerns)
 
         # Get stories that falls under category of ticked psychological concerns (and filter)
-        stories=db.execute("SELECT * FROM stories WHERE ")
+        stories=db.execute("SELECT * FROM stories WHERE psychological_concerns LIKE ?", "%" + x + "%")
 
         if not stories:
             flash("There are currently no stories, please revisit the page later")
