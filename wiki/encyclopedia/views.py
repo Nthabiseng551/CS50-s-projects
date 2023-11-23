f88e96001b508afcaf21e31462085f5ac23c5587
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-23 14:09:16 +0000

    Thu, Nov 23, 2023, 4:09 PM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -6,8 +6,10 @@ from markdown2 import Markdown
 
 # markdown2.markdown(content)
 def md_html(title):
-    content = util.get_entry(title)
-
+    markdowner = Markdown()
+    if util.get_entry(title) == None:
+        return None
+    return markdowner.convert(util.get_entry(title))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -15,14 +17,14 @@ def index(request):
     })
 
 def entry(request, title):
-
-     if util.get_entry(title) == None:
+    content = md_html(title)
+    if content == None:
         return render(request, "encyclopedia/error.html", {
             "message:" "The requested page not found"
         })
 
-     return render(request,"encyclopedia/entry.html", {
+    return render(request,"encyclopedia/entry.html", {
          "title": title,
-         "content": markdown2.markdown(util.get_entry(title))
+         "content": content
     })
 
