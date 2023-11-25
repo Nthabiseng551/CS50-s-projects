9690a482633411e0cd4f3c6be84ee27a1f18336c
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-25 08:27:28 +0000

    Sat, Nov 25, 2023, 10:27 AM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -46,7 +46,7 @@ def search(request):
         else:
             entries = util.list_entries()
             recommendation = []
-            for entry in entries:
+            for entry.capitalize in entries:
                 if search.upper() in entry.upper():
                     recommendation.append(entry)
             return render(request, "encyclopedia/search.html", {
