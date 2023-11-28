91f731f97aca4a26880fb7dd9063bdf9cd1d46e8
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-28 09:20:35 +0000

    Tue, Nov 28, 2023, 11:20 AM +02:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -70,7 +70,7 @@ class NewListingForm(forms.Form):
     title = forms.CharField(label="Title for listing", widget=forms.TextInput(attrs={'style': 'width: 300px;', 'class': 'form-control'}))
     description = forms.CharField(label="Description", widget=forms.TextInput(attrs={'style': 'width: 300px;', 'class': 'form-control'}))
     price = forms.FloatField(label="Price", widget=forms.TextInput(attrs={'style': 'width: 300px;', 'class': 'form-control'}))
-    image = forms.URLField(label="Enter image URL", required=False, widget=forms.TextInput(attrs={'style': 'width: 300px;', 'class': 'form-control'}))
+    image = forms.URLField(label="Enter image URL", required=False, widget=forms.URLInput(attrs={'style': 'width: 300px;', 'class': 'form-control'}))
     category = forms.CharField(label="Category", required=False, widget=forms.TextInput(attrs={'style': 'width: 300px;', 'class': 'form-control'}))
 
 #add login required decorator later
