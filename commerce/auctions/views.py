625c6433f2dbfa73a7a60b80898a17699d3cb640
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-28 09:23:28 +0000

    Tue, Nov 28, 2023, 11:23 AM +02:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django import forms
 from django.contrib.auth.decorators import login_required
-from django.forms import TextInput
+
 
 from .models import User
 
