1369df768bb9b1d6e25d8161b3d24effdbbcc87d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-28 13:21:10 +0000

    Tue, Nov 28, 2023, 3:21 PM +02:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("create", views.create_listing, name="create"),
-    path("auctions/<str:category>", views.listing, name="category")
+    path("create", views.create_listing, name="create")
 ]
