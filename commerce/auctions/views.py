463b92d2d95dfc230afcdea9471ca4d65a3ed6c6
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-28 09:53:29 +0000

    Tue, Nov 28, 2023, 11:53 AM +02:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -77,7 +77,28 @@ class NewListingForm(forms.Form):
 #add login required decorator later
 def create_listing(request):
     if request.method == "POST":
-        
+         # Take in the data the user submitted and save it as form
+        form = NewListingForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the variables from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
+            tasks.append(task)
+
+            # Redirect user to index
+            return HttpResponseRedirect(reverse("tasks:index"))
+
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
     else:
         return render(request, "auctions/create.html",{
             "form": NewListingForm()
