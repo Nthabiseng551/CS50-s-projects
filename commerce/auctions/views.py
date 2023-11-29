cd181bbe8636f2a20a800cdf105620a7b314c87b
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-29 15:32:40 +0000

    Wed, Nov 29, 2023, 5:32 PM +02:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -232,3 +232,4 @@ def close(request, listing_id):
     listing = Listing.objects.get(pk=listing_id)
     listing.active="no"
     listing.save()
+    message.success(request, 'Auction has been closed.')
