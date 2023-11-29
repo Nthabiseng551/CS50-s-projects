badafde2b5c27078c51dd7b4c0638fcab53ea726
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-29 14:23:37 +0000

    Wed, Nov 29, 2023, 4:23 PM +02:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("remove/<int:listing_id>", views.remove, name="remove"),
     path("add/<int:listing_id>", views.add, name="add"),
     path("watchlist", views.watchlist, name="watchlist"),
-    path("comment/<int:listing_id>", views.comment, name="comment")
+    path("comment/<int:listing_id>", views.comment, name="comment"),
+    path("bid/<int:listing_id>", views.bid, name="bid")
 
 ]
