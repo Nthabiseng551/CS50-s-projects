8863322031184c3c756e1dc9cf0e4b93cf50e2a0
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-29 11:26:35 +0000

    Wed, Nov 29, 2023, 1:26 PM +02:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,12 +9,12 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=100)
     price = models.FloatField()
-    image_url = models.URLField(blank=True)
+    image_url = models.URLField(blank=True, null=True)
     active = models.CharField(max_length=64, default="yes")
-    category = models.CharField(max_length=64, blank=True)
+    category = models.CharField(max_length=64, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     listed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
-    watchlist = models.ManyToManyField(User, blank=True, related_name="user")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="user")
 
     def __str__(self):
         return f"{self.title}"
