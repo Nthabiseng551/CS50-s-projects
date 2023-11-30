dd61670672e0d9fe76454f8fb2d96be66cb5469d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-11-30 08:43:29 +0000

    Thu, Nov 30, 2023, 10:43 AM +02:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -233,5 +233,5 @@ def close(request, listing_id):
     listing.active="no"
     listing.save()
     message.success(request, 'Auction has been closed.')
-    if listing.active=="no" and user=listing.price.user:
+    if listing.active=="no" and user==listing.price.user:
         message.success(request, 'Congratulations! you have won the auction')
