cbf3d78b2ee5feae0527ec5b77b63c6c47086d3e
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-01 16:10:39 +0000

    Fri, Dec 1, 2023, 6:10 PM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -19,6 +19,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#emails-content').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -39,7 +40,8 @@ function view_email(id){
       document.querySelector('#emails-content').style.display = 'block';
 
       document.querySelector('#emails-content').innerHTML = `
-          <p>$</p>
+          <p>${email.sender}: ${email.subject} : ${email.timestamp} : ${email.recipient}</p>
+          <p>${email.body}</p>
       `;
   });
 }
@@ -49,6 +51,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#emails-content').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
