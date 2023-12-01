7c52e498af1364241a2d59ba30a54e728c9161a8
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-01 14:09:27 +0000

    Fri, Dec 1, 2023, 4:09 PM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -8,7 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // When Compose form is submitted
   document.querySelector('#compose-form').addEventListener('onsubmit', () =>{
-    
+    // Get info submitted
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
+
+    // send email
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: recipients,
+          subject: subject,
+          body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+        load_mailbox('sent');
+        
   }) = () => {
     // Get info submitted
     const recipients = document.querySelector('#compose-recipients').value;
