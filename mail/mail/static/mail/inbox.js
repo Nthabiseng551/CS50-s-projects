445cd2878aaa31252910b4c869a0e399c49b13ac
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-03 04:37:30 +0000

    Sun, Dec 3, 2023, 6:37 AM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -32,6 +32,16 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get emails for mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+      console.log(emails);
+
+    // ... do something else with emails ...
+  });
 }
 
 function send_email() {
