cf1b7de05f408a88df9bd81b9dd6c3feb61d2254
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-03 06:36:48 +0000

    Sun, Dec 3, 2023, 8:36 AM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -75,7 +75,7 @@ function view_email(id) {
                 archived: false
             })
           })
-          .then () => load_mailbox('archive');
+          .then () => load_mailbox('inbox');
         }
         else {
           fetch(`/emails/${email.id}`, {
