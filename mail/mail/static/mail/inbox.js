2f0ba98ec8a86a9d0a736ef243a51ef1a4542c8e
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-03 04:17:16 +0000

    Sun, Dec 3, 2023, 6:17 AM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -33,3 +33,8 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function send_email() {
+  console.log("hi");
+  return false;
+}
