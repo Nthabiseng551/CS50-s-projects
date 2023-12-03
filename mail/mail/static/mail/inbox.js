6474b872fe8fa0a945dd6afa1d2d23791086b7c1
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-03 09:19:14 +0000

    Sun, Dec 3, 2023, 11:19 AM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -96,7 +96,15 @@ function view_email(id) {
       reply.innerHTML = 'Reply';
       document.querySelector('#email-content').append(reply);
       reply.addEventListener('click', function(){
-        
+        // Show compose view and hide other views
+        document.querySelector('#emails-view').style.display = 'none';
+        document.querySelector('#compose-view').style.display = 'block';
+        document.querySelector('#email-content').style.display = 'none';
+
+        // Pre-fill composition fields
+        document.querySelector('#compose-recipients').value = email.sender;
+        document.querySelector('#compose-subject').value = '';
+        document.querySelector('#compose-body').value = email.body;
       });
       });
 }
