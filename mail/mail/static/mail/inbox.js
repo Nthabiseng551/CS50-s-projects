4b6261e8c2c81455509b86e936e26beaead93fac
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-03 05:04:57 +0000

    Sun, Dec 3, 2023, 7:04 AM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -44,7 +44,7 @@ function load_mailbox(mailbox) {
     emails.forEach(email => {
       const mail = document.createElement('div');
       mail.className = "list-group-item";
-      mail.innerHTML = `<p><strong>${email.sender}</strong> ${email.subject}</p> <p class="text-muted">${email.timestamp}</p>`;
+      mail.innerHTML = `<hr><p><strong>${email.sender}</strong> ${email.subject}<span class="text-muted">${email.timestamp}</p><hr>`;
       mail.addEventListener('click', function() {
           console.log('This element has been clicked!')
       });
