d9ea88f5789f15368f782c0ed2a348d7d1000b1d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-03 08:57:52 +0000

    Sun, Dec 3, 2023, 10:57 AM +02:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -57,7 +57,7 @@ function view_email(id) {
         })
       }
       // archive vs unarchive (not applicable to sent mailbox)
-      if (email.user === email.sender){
+
           const archive = document.createElement('button');
           if (email.archived){
               archive.className = "btn btn-sm btn-outline-primary";
@@ -89,7 +89,7 @@ function view_email(id) {
           }
         });
           document.querySelector('#email-content').append(archive);
-                        }
+
       });
 }
 
