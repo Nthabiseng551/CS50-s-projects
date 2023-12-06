ce1d26b9d13c18b1a1228b83f2caeb2545ba67df
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-06 12:44:03 +0000

    Wed, Dec 6, 2023, 2:44 PM +02:00

--- project4/network/models.py
+++ project4/network/models.py
@@ -5,5 +5,14 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class post
+class Post(models.Model):
 //user, content, timestamp
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=300)
+    comment_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="item")
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return f"comment on post,{self.post}, by {self.comment_by}"
