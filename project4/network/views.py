b025bafe924d30b7bd84a9e76203f7a707bb16aa
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-07 06:13:18 +0000

    Thu, Dec 7, 2023, 8:13 AM +02:00

--- project4/network/views.py
+++ project4/network/views.py
@@ -91,7 +91,7 @@ def new_post(request):
 
 def profile(request, user_id):
     user = User.objects.get(pk=user_id)
-
+    username = user.username
     posts = Post.objects.filter(user=user).order_by("timestamp").reverse()
     # Paginator (10 posts per page)
     paginator = Paginator(posts, 10)
@@ -112,8 +112,9 @@ def profile(request, user_id):
 
     return render(request, "network/profile.html", {
         "page_posts": page_posts,
-        "user": user,
+        "username": username,
         "following": len(following),
         "followers": len(followers),
-        "isFollower": isFollower
+        "isFollower": isFollower,
+        "user": user
     })
