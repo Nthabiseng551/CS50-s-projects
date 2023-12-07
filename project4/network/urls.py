b756e29c6ffc0942e09d6c0876ea8588f39b6ec3
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-07 04:54:10 +0000

    Thu, Dec 7, 2023, 6:54 AM +02:00

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("new", views.new_post, name="new")
+    path("new", views.new_post, name="new"),
+    path("profile/<int:user_id>", views.profile, name="profile")
 ]
