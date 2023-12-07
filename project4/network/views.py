f8c8339a63d829a6d688c7f2c703eeba8ea24eb6
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-07 09:25:43 +0000

    Thu, Dec 7, 2023, 11:25 AM +02:00

--- project4/network/views.py
+++ project4/network/views.py
@@ -22,13 +22,14 @@ def index(request):
     #check if current user liked a post
     for like in likes:
             if like.user.id == request.user.id:
-                isFollower = True
+                liked.append(like.post.id)
                 break
             else:
-                isFollower = False
+                liked = []
 
     return render(request, "network/index.html", {
-        "page_posts": page_posts
+        "page_posts": page_posts,
+        "liked": liked
     })
 
 
