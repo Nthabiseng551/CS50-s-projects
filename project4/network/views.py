71501edc054718e1658672ee7640a41f3f6bad0a
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-07 05:50:47 +0000

    Thu, Dec 7, 2023, 7:50 AM +02:00

--- project4/network/views.py
+++ project4/network/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from datetime import datetime
-from .models import User, Post, Comment
+from .models import User, Post, UserFollowing
 from django.core.paginator import Paginator
 
 
