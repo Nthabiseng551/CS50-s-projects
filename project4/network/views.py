065e5810fe26df1001913aeb3cc853627fbf8842
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-11 13:25:20 +0000

    Mon, Dec 11, 2023, 3:25 PM +02:00

--- project4/network/views.py
+++ project4/network/views.py
@@ -114,8 +114,8 @@ def profile(request, user_id):
     page_posts = paginator.get_page(page_number)
 
     # Following and followers
-    following = userProfile.following.all()
-    followers = userProfile.follower.all()
+    following = userProfile.follower.all()
+    followers = userProfile.following.all()
 
     #check if current user is a follower
     try:
