1d2c573e6e29da399fab9bccfca29b2de18b0d4a
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-12 13:35:14 +0000

    Tue, Dec 12, 2023, 3:35 PM +02:00

--- project4/network/views.py
+++ project4/network/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
