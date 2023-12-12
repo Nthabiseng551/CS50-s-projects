8c4d1087c1aeb116b2139a3706f105d83d11db20
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-12 17:07:58 +0000

    Tue, Dec 12, 2023, 7:07 PM +02:00

--- project4/network/static/network/network.js
+++ project4/network/static/network/network.js
@@ -26,4 +26,20 @@ document.addEventListener('DOMContentLoaded', function() {
   })
   document.querySelector('#liked-btn').onclick = count;
   document.querySelector('#unlike-btn').onclick = count;
+
 })
+
+var clicks = 0;
+$(".like_cont").click(function(e) {
+    if ($(this).html() == "Like") {
+        $(this).html('Unlike').removeClass('like_cont').addClass('unlike_cont');
+        clicks++;
+       $('.likecount').html(clicks);
+    }
+    else {
+    $(this).html('Like').removeClass('unlike_cont').addClass('like_cont');
+        clicks--;
+        $('.likecount').html(clicks);
+    }
+    return false;
+});
