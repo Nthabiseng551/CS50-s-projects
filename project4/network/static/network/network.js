c06740010e861c88c352522f91967499b400e2be
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-12 17:37:50 +0000

    Tue, Dec 12, 2023, 7:37 PM +02:00

--- project4/network/static/network/network.js
+++ project4/network/static/network/network.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch(`/like/${id}`)
     .then(response => response.json())
     .then(result => {
-        
+        document.querySelector('#post-content').innerHTML = result.num_likes;
     });
   })
 
