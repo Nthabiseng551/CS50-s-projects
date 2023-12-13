ceb460190923c4995b8cce0b39934d4990b90c4d
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-13 05:44:52 +0000

    Wed, Dec 13, 2023, 7:44 AM +02:00

--- project4/network/static/network/like.js
+++ project4/network/static/network/like.js
@@ -23,23 +23,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-function liking(id)
-    const likeBtn
-
-    if (event.target.className === "btn btn-danger"){
-        fetch(`/unlike/${post.id}`)
-        .then(response => response.json)
-        .then(result => {
-            console.log(result.num_likes);
-            likeBtn.className === "btn btn-dark";
-        })
-    }
-    else if (event.target.className === "btn btn-dark"){
-        fetch(`/like/${post.id}`)
-        .then(response => response.json)
-        .then(result => {
-            console.log(result.num_likes);
-            likeBtn.className === "btn btn-danger";
-        })
-    }
-})
