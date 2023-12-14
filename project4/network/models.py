9bb99a1ddb35b7bdd955fb5e1ea54f11a77b8cf1
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-14 08:38:13 +0000

    Thu, Dec 14, 2023, 10:38 AM +02:00

--- project4/network/models.py
+++ project4/network/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     post = models.CharField(max_length=300)
     post_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    likes = models.ManyToManyField(User, blank=True, related_name='likes')
 
     def __str__(self):
         return f"Post {self.id} by,{self.post_by}"
