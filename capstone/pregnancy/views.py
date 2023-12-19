b72b7ce41fc094d696e8b301db089137a42d806e
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-19 22:46:55 +0000

    Wed, Dec 20, 2023, 12:46 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+from datetime import datetime
+
+from .models import User,
 # Create your views here.
