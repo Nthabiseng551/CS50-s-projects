b5595651205c1d09574d1e40a2a296dd1b31fe1e
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-19 22:55:38 +0000

    Wed, Dec 20, 2023, 12:55 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -26,11 +26,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "network/login.html", {
+            return render(request, "pregnancy/login.html", {
                 "message": "Invalid username and/or password."
             })
     else:
-        return render(request, "network/login.html")
+        return render(request, "pregnancy/login.html")
 
 
 def logout_view(request):
@@ -47,7 +47,7 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "network/register.html", {
+            return render(request, "pregnancy/register.html", {
                 "message": "Passwords must match."
             })
 
@@ -56,7 +56,7 @@ def register(request):
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "network/register.html", {
+            return render(request, "pregnancy/register.html", {
                 "message": "Username already taken."
             })
         login(request, user)
