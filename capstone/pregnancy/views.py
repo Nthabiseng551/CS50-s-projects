fb8d630fefe5c9b221cc06418d4651a835fdcb4a
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-19 22:50:57 +0000

    Wed, Dec 20, 2023, 12:50 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 from datetime import datetime
 
