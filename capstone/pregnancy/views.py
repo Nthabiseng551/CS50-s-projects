d102eb80af616682cff80d612cf4af07c52ef451
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-20 01:51:12 +0000

    Wed, Dec 20, 2023, 3:51 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -14,6 +14,7 @@ from .models import User, UserProfile
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
     currentUser = User.objects.get(pk=request.user.id)
     userProfile = UserProfile.objects.filter(user=currentUser)
     return render(request, "pregnancy/index.html")
