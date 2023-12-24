11da6c8adaa88c76cab9e60505af9f65b07d1657
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-24 21:31:35 +0000

    Sun, Dec 24, 2023, 11:31 PM +02:00

--- capstone/pregnancy/urls.py
+++ capstone/pregnancy/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
      path("login", views.login_view, name="login"),
      path("logout", views.logout_view, name="logout"),
      path("register", views.register, name="register"),
-     path("proffesionals", views.professionals, name="professionals")
+     path("profesionals", views.professionals, name="professionals"),
+     path("counsellor", views.counsellor, name="counsellor")
  ]
