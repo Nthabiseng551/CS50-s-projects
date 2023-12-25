86ff6d9a62d559a9837b93190ec5c1078102f35f
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-25 07:38:46 +0000

    Mon, Dec 25, 2023, 9:38 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -172,7 +172,9 @@ def health(request):
     if week > 0 and week < 41:
         for i in range(week, 41):
             if currentdate = updateddate + timedelta(weeks=4)
+                
                 week = i
+
     return render(request, "pregnancy/health.html",{
         "period": period
     })
