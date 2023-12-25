6c0cf5fa8a791935ad7239362aeef03d059b2c6f
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-25 06:35:55 +0000

    Mon, Dec 25, 2023, 8:35 AM +02:00

--- capstone/pregnancy/models.py
+++ capstone/pregnancy/models.py
@@ -11,6 +11,7 @@ class UserProfile(models.Model):
     dietician = models.BooleanField(default=False)
     counsellor = models.BooleanField(default=False)
     week_of_pregnancy = models.IntegerField(null=True)
+    week_date
     pre_weight = models.FloatField(null=True, blank=True)
     target_weight = models.FloatField(null=True, blank=True)
     current_weight = models.FloatField(null=True, blank=True)
