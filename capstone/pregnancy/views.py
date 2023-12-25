9919d86cc20da50cbb41bf26935dd6e2e8522eae
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-25 09:06:08 +0000

    Mon, Dec 25, 2023, 11:06 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -167,6 +167,7 @@ def health(request):
     deltaa = time_delta.days
     weeks = math.floor(deltaa/7) + week
     userProfile.week_of_pregnancy = weeks
+    userProfile.week_update_date =
     userProfile.save()
 
 
