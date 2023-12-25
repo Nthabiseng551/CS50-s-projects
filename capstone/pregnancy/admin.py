295ba6eb32b5e3e82b1e1c28df6384b6da4857da
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-25 12:35:17 +0000

    Mon, Dec 25, 2023, 2:35 PM +02:00

--- capstone/pregnancy/admin.py
+++ capstone/pregnancy/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, UserProfile
+from .models import User, UserProfile, Test, UserTest
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(Test)
+admin.site.register(UserTest)
