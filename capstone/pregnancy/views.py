81433432596e7c5a27dead4cf62a07d0906e5295
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-25 05:09:31 +0000

    Mon, Dec 25, 2023, 7:09 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -11,7 +11,10 @@ from datetime import datetime
 
 from .models import User, UserProfile
 
+#incrementing weeks of pregnancy
+for i in range(1, 41)
 # Create your views here.
+
 @csrf_exempt
 def index(request):
 
