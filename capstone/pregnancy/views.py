a7469b207c1f7dcc71fd62e88da75da6c335e6bc
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-25 07:55:35 +0000

    Mon, Dec 25, 2023, 9:55 AM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -167,7 +167,7 @@ def health(request):
     userProfile = UserProfile.objects.get(user=currentUser)
     week = userProfile.week_of_pregnancy
     updated_date = userProfile.week_update_date
-    current_date = datetime.now() + timedelta(weeks=2)
+    current_date = datetime.now() 
 
     if week > 0 and week < 41:
         for i in range(week, 41):
