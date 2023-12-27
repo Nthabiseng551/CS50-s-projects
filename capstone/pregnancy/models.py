0d70619688d511bf053b9c695c18abe0a099b2ad
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-27 15:32:40 +0000

    Wed, Dec 27, 2023, 5:32 PM +02:00

--- capstone/pregnancy/models.py
+++ capstone/pregnancy/models.py
@@ -17,12 +17,12 @@ class UserProfile(models.Model):
     target_weight = models.FloatField(null=True, blank=True)
     current_weight = models.FloatField(null=True, blank=True)
     currentWeight_update_date = models.DateField(default=date.today)
+    tests = models.ManyToManyField("Test", null=True, blank=True, related_name='users')
 
     def __str__(self):
         return f"{self.id}, {self.user}, {self.week_of_pregnancy}"
 
 class Test(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=True, related_name="tests")
     test_name = models.CharField(max_length=100)
     description = models.CharField(max_length=255, null=True, blank=True)
     trimester = models.IntegerField(null=True)
