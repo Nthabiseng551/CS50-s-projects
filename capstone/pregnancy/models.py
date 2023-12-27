87258b71c6df38937c3a276e5b1f055c33d8a6ca
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-27 15:09:28 +0000

    Wed, Dec 27, 2023, 5:09 PM +02:00

--- capstone/pregnancy/models.py
+++ capstone/pregnancy/models.py
@@ -22,7 +22,7 @@ class UserProfile(models.Model):
         return f"{self.id}, {self.user}, {self.week_of_pregnancy}"
 
 class Test(models.Model):
-    user = models.ForeignKey("User", null=True, blank=True, related_name="tests")
+    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=True, related_name="tests")
     test_name = models.CharField(max_length=100)
     description = models.CharField(max_length=255, null=True, blank=True)
     trimester = models.IntegerField(null=True)
