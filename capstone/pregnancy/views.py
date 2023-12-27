4e38b9598bd010ef341b9c629c4467fb0425f9c5
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-27 15:12:12 +0000

    Wed, Dec 27, 2023, 5:12 PM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -100,6 +100,8 @@ def register(request):
                 "message": "Username already taken."
             })
 
+        tests = Test.objects.all()
+        for test in tests:
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
