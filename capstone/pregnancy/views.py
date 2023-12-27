5cc581db80422f983e7fdf1f5f2961d0580c93fe
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-27 18:41:55 +0000

    Wed, Dec 27, 2023, 8:41 PM +02:00

--- capstone/pregnancy/views.py
+++ capstone/pregnancy/views.py
@@ -246,6 +246,5 @@ def tests(request):
     return render(request, "pregnancy/tests.html", {
         "tests1": tests1,
         "tests2": tests2,
-        "tests3": tests3,
-        "done": False
+        "tests3": tests3
     })
