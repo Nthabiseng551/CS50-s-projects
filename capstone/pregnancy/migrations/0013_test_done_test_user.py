eed368e54e3b610ae7cd21e90e79727d419e34e2
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-27 10:30:08 +0000

    Wed, Dec 27, 2023, 12:30 PM +02:00

--- capstone/pregnancy/migrations/0013_test_done_test_user.py
+++ capstone/pregnancy/migrations/0013_test_done_test_user.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
             model_name="test",
             name="user",
             field=models.ForeignKey(
-                default=,
+                default=1,
                 on_delete=django.db.models.deletion.CASCADE,
                 related_name="tests",
                 to=settings.AUTH_USER_MODEL,
