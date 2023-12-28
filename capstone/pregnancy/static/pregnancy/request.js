f0c48ac9df739c02a829ec88d3099d780bc5fe98
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-28 11:10:53 +0000

    Thu, Dec 28, 2023, 1:10 PM +02:00

--- capstone/pregnancy/static/pregnancy/request.js
+++ capstone/pregnancy/static/pregnancy/request.js
@@ -5,3 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#diet-form').onsubmit = send_request;
 
   });
+
+function diet_request() {
+
+    // Show compose view and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'block';
+    document.querySelector('#email-content').style.display = 'none';
+
+    // Clear out composition fields
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
+  }
