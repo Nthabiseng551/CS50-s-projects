24f437bae81ca0ec5e5ba9103f8dcaa98547dcd1
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-28 08:01:36 +0000

    Thu, Dec 28, 2023, 10:01 AM +02:00

--- capstone/pregnancy/models.py
+++ capstone/pregnancy/models.py
@@ -33,7 +33,6 @@ class Test(models.Model):
 
 class Request(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
-    sender = models.ForeignKey("User", on_delete=models.PROTECT, related_name="emails_sent")
     recipients = models.ManyToManyField("User", related_name="emails_received")
     subject = models.CharField(max_length=255)
     body = models.TextField(blank=True)
