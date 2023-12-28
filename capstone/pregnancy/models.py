e4a8f966511896a53d64ac08c3cafa1050172b34
Nthabiseng551 <142518292+Nthabiseng551@users.noreply.github.com>
2023-12-28 11:55:40 +0000

    Thu, Dec 28, 2023, 1:55 PM +02:00

--- capstone/pregnancy/models.py
+++ capstone/pregnancy/models.py
@@ -18,6 +18,8 @@ class UserProfile(models.Model):
     current_weight = models.FloatField(null=True, blank=True)
     currentWeight_update_date = models.DateField(default=date.today)
     tests = models.ManyToManyField("Test", null=True, blank=True, related_name='usertests')
+    counselling_requested = models.BooleanField(default=False)
+    diet_requested = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.id}, {self.user}, {self.week_of_pregnancy}"
